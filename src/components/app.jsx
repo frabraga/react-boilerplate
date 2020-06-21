3cc977ebac39bdd417cd7fe0fbe2582e2b54aae8
Francisco Braga <franciscobragafernandes@gmail.com>
2020-06-21 12:11:13 -0300

    Create gif, gif_list and search_bar components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import SearchBar from "./search_bar";
+import Gif from "./gif";
+import GifList from "./gif_list";
 
 class App extends Component {
   render() {
+    const gifs = [{ id: "xT9IgDEI1iZyb2wqo8" }, { id: "3ohzdIrmkBViO8uouc" }];
     return (
       <div>
-        <div className="left-scene"></div>
-        <div className="right-scene"></div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id="xT9IgDEI1iZyb2wqo8" />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
